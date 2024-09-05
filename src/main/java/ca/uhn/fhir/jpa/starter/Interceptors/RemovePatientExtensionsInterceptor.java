d9d2126afedc58e5b3b8306c54641a03f1877251
Thomas <54805624+thomas-samoht@users.noreply.github.com>
2024-09-05 14:39:20 +0200

    Also remove extension when patient is in a bundle :P

--- src/main/java/ca/uhn/fhir/jpa/starter/Interceptors/RemovePatientExtensionsInterceptor.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/Interceptors/RemovePatientExtensionsInterceptor.java
@@ -3,7 +3,9 @@ package ca.uhn.fhir.jpa.starter.Interceptors;
 import ca.uhn.fhir.interceptor.api.Hook;
 import ca.uhn.fhir.interceptor.api.Interceptor;
 import ca.uhn.fhir.interceptor.api.Pointcut;
+import org.hl7.fhir.BundleEntry;
 import org.hl7.fhir.instance.model.api.IBaseResource;
+import org.hl7.fhir.r5.model.Bundle;
 import org.hl7.fhir.r5.model.Patient;
 
 import java.util.Collections;
@@ -13,11 +15,20 @@ public class RemovePatientExtensionsInterceptor {
 
 	@Hook(Pointcut.SERVER_OUTGOING_RESPONSE)
 	public void removeExtensions(IBaseResource resource) {
-		if (resource instanceof Patient) {
-			Patient patient = (Patient) resource;
-
+		if (resource instanceof Patient patient) {
 			if (patient.hasExtension()) {
 				patient.setExtension(Collections.emptyList());
+			}
+		}
+		if (resource instanceof Bundle bundle) {
+			if (bundle.hasEntry()) {
+				for (Bundle.BundleEntryComponent entry: bundle.getEntry()){
+					if (entry.getResource() instanceof Patient patient) {
+						if (patient.hasExtension()) {
+							patient.setExtension(Collections.emptyList());
+						}
+					}
+				}
 			}
 		}
 	}
